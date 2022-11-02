11254110b853a0b60a4ad615181086b8fccb2a74
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-02 08:20:26 -0400

    fix missing buttons and return all responses

--- src/slackUtils/slackPostMessage.ts
+++ src/slackUtils/slackPostMessage.ts
@@ -1,7 +1,7 @@
 import { WebClient } from '@slack/web-api'
 
 import slackBuilder from '../slackBuilder'
-import { TBlock, TBlocks } from '../types'
+import { TBlock, TBlockElements, TBlocks } from '../types'
 
 import slackUpdateMessage from './slackUpdateMessage'
 
@@ -16,33 +16,57 @@ const slackPostMessage = async (
 ) => {
     const web = new WebClient(token)
     try {
-        const result = await web.chat.postMessage({
+        const resultPostMessage = await web.chat.postMessage({
             channel,
             text,
             blocks
         })
-        console.log('\x1b[34m%s\x1b[0m', 'Message sent: ', result.ts, addDeleteBtn, addSettings)
+        console.log('\x1b[34m%s\x1b[0m', 'Message sent: ', resultPostMessage.ts, addDeleteBtn, addSettings)
 
-        let actionBlock = blocks.filter((block: TBlock) => block.type === 'actions')
-        let actionBlockElements = actionBlock[0].elements
-        if (actionBlock.length === 0) actionBlock = []
-        if (actionBlockElements === undefined || actionBlockElements.length === 0) actionBlockElements = []
+        const defaultActionBlock = {
+            type: 'actions',
+            block_id: 'actions1',
+            elements: []
+        }
+        let actionBlock: TBlock[] | undefined
+        let actionBlockElements: TBlockElements | undefined
 
-        if (actionBlock !== undefined && actionBlockElements !== undefined && result.ts !== undefined) {
-            if (addDeleteBtn) actionBlockElements.push(slackBuilder.addDeleteButton(actionBlockElements, result.ts)[0])
-            if (addSettings) actionBlockElements.push(slackBuilder.addSettingButton(actionBlockElements, result.ts)[0])
-            if (addRefresh) actionBlockElements.push(slackBuilder.addRefreshButton(actionBlockElements, result.ts)[0])
+        try {
+            actionBlock = blocks.filter((block: TBlock) => block.type === 'actions')
+            if (actionBlock.length === 0) actionBlock = [defaultActionBlock]
+        } catch (err) {
+            actionBlock = [defaultActionBlock]
+        }
+
+        try {
+            actionBlockElements = actionBlock[0].elements
+        } catch (err) {
+            actionBlockElements = []
+        }
+
+        let resultUpdateMessage: any
+        if (actionBlock !== undefined && actionBlockElements !== undefined && resultPostMessage.ts !== undefined) {
+            if (addDeleteBtn)
+                actionBlockElements = slackBuilder.addDeleteButton(actionBlockElements, resultPostMessage.ts)
+            if (addSettings)
+                actionBlockElements = slackBuilder.addSettingButton(actionBlockElements, resultPostMessage.ts)
+            if (addRefresh)
+                actionBlockElements = slackBuilder.addRefreshButton(actionBlockElements, resultPostMessage.ts)
             if (addDeleteBtn || addSettings || addRefresh) {
                 blocks = [
                     ...blocks.filter((block: TBlock) => block.type !== 'actions'),
                     {
-                        type: 'actions',
+                        type: defaultActionBlock.type,
                         block_id: actionBlock[0].block_id,
                         elements: actionBlockElements
                     }
                 ]
-                await slackUpdateMessage(token, channel, text, result.ts, blocks)
+                resultUpdateMessage = await slackUpdateMessage(token, channel, text, resultPostMessage.ts, blocks)
             }
+        }
+        const result = {
+            resultPostMessage,
+            resultUpdateMessage
         }
         return result
     } catch (error) {
