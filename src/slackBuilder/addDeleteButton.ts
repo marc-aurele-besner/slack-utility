d9ffc064a816bc4078a80e641a1c99273aec0bc3
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-02 08:19:50 -0400

    improve typing and edge case

--- src/slackBuilder/addDeleteButton.ts
+++ src/slackBuilder/addDeleteButton.ts
@@ -1,10 +1,12 @@
-import { TBlockElements } from '../types'
+import { TBlockElement, TBlockElements } from '../types'
 
 import buildSimpleSlackButton from './buildSimpleSlackButton'
 
-const addDeleteButton = (elements: TBlockElements, ts: string): TBlockElements => {
-    const deleteIsPresent = elements.find((element: any) => element.action_id === 'delete_msg')
-    if (!deleteIsPresent) elements.push(buildSimpleSlackButton('Delete', ts, 'delete_msg', 'danger'))
+const addDeleteButton = (elements: TBlockElements | undefined, ts: string): TBlockElements => {
+    if (elements === undefined) elements = []
+    const deleteIsPresent = elements.find((element: TBlockElement) => element.action_id === 'delete_msg')
+    if (!deleteIsPresent || deleteIsPresent === undefined)
+        elements.push(buildSimpleSlackButton('Delete', ts, 'delete_msg', 'danger'))
     return elements
 }
 
--- src/slackBuilder/addRefreshButton.ts
+++ src/slackBuilder/addRefreshButton.ts
@@ -1,10 +1,11 @@
-import { TBlockElements } from '../types'
+import { TBlockElement, TBlockElements } from '../types'
 
 import buildSimpleSlackButton from './buildSimpleSlackButton'
 
 const addRefreshButton = (elements: TBlockElements, ts: string): TBlockElements => {
-    const refreshIsPresent = elements.find((element: any) => element.action_id === 'refresh')
-    if (!refreshIsPresent)
+    if (elements === undefined) elements = []
+    const refreshIsPresent = elements.find((element: TBlockElement) => element.action_id === 'refresh')
+    if (!refreshIsPresent || refreshIsPresent === undefined)
         elements.push(
             buildSimpleSlackButton(':repeat:', JSON.stringify({ action: 'refresh', ts }), 'refresh', undefined)
         )
