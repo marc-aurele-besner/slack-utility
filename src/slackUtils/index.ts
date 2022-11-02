f5c7352da50db673fa5e674ed8f7c101191e0766
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-02 09:15:13 -0400

    add beta callerSettings

--- src/slackUtils/index.ts
+++ src/slackUtils/index.ts
@@ -1,8 +1,10 @@
+import callerSettings from './callerSettings'
 import slackDeleteMessage from './slackDeleteMessage'
 import slackPostMessage from './slackPostMessage'
 import slackUpdateMessage from './slackUpdateMessage'
 
 const slackUtils = {
+    callerSettings,
     slackDeleteMessage,
     slackPostMessage,
     slackUpdateMessage
