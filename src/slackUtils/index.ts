66ab0d08e3a961243e1a17e693aa74bcbfb5db50
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-15 07:48:36 -0500

    add new helper functions

--- src/slackUtils/index.ts
+++ src/slackUtils/index.ts
@@ -1,32 +1,41 @@
 import actionsLoop from './actionsLoop'
 import callerSettings from './callerSettings'
+import commandsLoop from './commandsLoop'
 import retrieveEnvironment from './retrieveEnvironment'
 import retrieveSettings from './retrieveSettings'
 import setupContractAndNetwork from './setupContractAndNetwork'
 import setupContractNetworkAndSigner from './setupContractNetworkAndSigner'
 import setupNetwork from './setupNetwork'
+
 import slackDeleteMessage from './slackDeleteMessage'
+import slackOpenView from './slackOpenView'
+import slackPostEphemeralMessage from './slackPostEphemeralMessage'
 import slackPostMessage from './slackPostMessage'
 import slackPostWaitMessage from './slackPostWaitMessage'
 import slackPublishView from './slackPublishView'
+import slackPushView from './slackPushView'
 import slackUpdateMessage from './slackUpdateMessage'
-import slashCommandsLoop from './slashCommandsLoop'
+import slackUpdateView from './slackUpdateView'
 
 const slackUtils = {
     actionsLoop,
     callerSettings,
+    commandsLoop,
     retrieveEnvironment,
     retrieveSettings,
     setupContractAndNetwork,
     setupContractNetworkAndSigner,
     setupNetwork,
-    slashCommandsLoop,
 
     slackDeleteMessage,
+    slackOpenView,
+    slackPostEphemeralMessage,
     slackPostMessage,
     slackPostWaitMessage,
     slackPublishView,
-    slackUpdateMessage
+    slackPushView,
+    slackUpdateMessage,
+    slackUpdateView
 }
 
 export default slackUtils
