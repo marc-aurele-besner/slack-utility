994c6df786dae4a1df0750110dcc35addeea5dd1
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-15 07:46:14 -0500

    rename slashCommandsLoop commandsLoop

--- src/slackUtils/commandsLoop.ts
+++ src/slackUtils/commandsLoop.ts
@@ -6,7 +6,7 @@ const defaultValues: TEnv = {
     commands: undefined as TCommand[] | undefined
 }
 
-const slashCommandsLoop = async (env = defaultValues as TEnv | undefined, parsedBody: any) => {
+const commandsLoop = async (env = defaultValues as TEnv | undefined, parsedBody: any) => {
     if (parsedBody.command !== undefined && env !== undefined && env.commands !== undefined) {
         env?.commands.forEach((command) => {
             if (command.command === parsedBody.command)
@@ -21,4 +21,4 @@ const slashCommandsLoop = async (env = defaultValues as TEnv | undefined, parsed
     return parsedBody
 }
 
-export default slashCommandsLoop
+export default commandsLoop
