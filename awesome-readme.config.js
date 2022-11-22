05868906fc507f34894fde9ff48e36fc9e992e03
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-11-22 09:58:48 -0500

    Update awesome-readme.config.js

--- awesome-readme.config.js
+++ awesome-readme.config.js
@@ -5,5 +5,23 @@ module.exports = {
     \`8bo.   88      88ooo88 8P      88,8P          88    88    88       88    88         88       88     \`8bd8'  
       \`Y8b. 88      88~~~88 8b      88\`8b   C8888D 88    88    88       88    88         88       88       88    
     db   8D 88booo. 88   88 Y8b  d8 88 \`88.        88b  d88    88      .88.   88booo.   .88.      88       88    
-    \`8888Y' Y88888P YP   YP  \`Y88P' YP   YD        ~Y8888P'    YP    Y888888P Y88888P Y888888P    YP       YP    `
+    \`8888Y' Y88888P YP   YP  \`Y88P' YP   YD        ~Y8888P'    YP    Y888888P Y88888P Y888888P    YP       YP    `,
+    root_license: `[![npm version](https://badge.fury.io/js/slack-utility.svg)](https://badge.fury.io/js/slack-utility)`,
+    root_header: `
+## Installation
+\`\`\`
+npm install slack-utility
+\`\`\`
+or
+\`\`\`
+yarn add slack-utility
+\`\`\`
+`,
+    root_body: `
+## Usage
+  `,
+    root_footer: `## Don't hesitate to contribute to this project.`,
+    ignore_gitFiles: true,
+    ignore_gitIgnoreFiles: true,
+    ignore_files: []
 }
